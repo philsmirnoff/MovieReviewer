64878f2e3f246ad26e42edb38842a7e2db584b45
Ilfir Yakupov <philsmirnoff@gmail.com>
2024-12-06 00:21:49 -0500

    Solved the issue of getting an array of empty objects by adding getters and setters to Movie class

--- backend/src/main/java/ilfiryakupov/dev/backend/Movie.java
+++ backend/src/main/java/ilfiryakupov/dev/backend/Movie.java
@@ -1,6 +1,6 @@
 package ilfiryakupov.dev.backend;
+
 import lombok.AllArgsConstructor;
-import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.bson.types.ObjectId;
 import org.springframework.data.annotation.Id;
@@ -13,9 +13,9 @@ import java.util.List;
  * Represents a Movie document in the MongoDB database.
  */
 @Document(collection = "movies")
-@Data
-@AllArgsConstructor
 @NoArgsConstructor
+@AllArgsConstructor
+
 public class Movie {
     @Id
     private ObjectId id;
@@ -28,6 +28,98 @@ public class Movie {
     private List<String> backdrops;
     @DocumentReference
     private List<Review> reviewIds;
+
+    // Default constructor
+    public Movie() {
+    }
+
+    // Parameterized constructor
+    public Movie(ObjectId id, String imdbId, String title, String releaseDate, String trailerLink, String poster,
+                 List<String> genres, List<String> backdrops, List<Review> reviewIds) {
+        this.id = id;
+        this.imdbId = imdbId;
+        this.title = title;
+        this.releaseDate = releaseDate;
+        this.trailerLink = trailerLink;
+        this.poster = poster;
+        this.genres = genres;
+        this.backdrops = backdrops;
+        this.reviewIds = reviewIds;
+    }
+
+    // Getters and Setters
+    public ObjectId getId() {
+        return id;
+    }
+
+    public void setId(ObjectId id) {
+        this.id = id;
+    }
+
+    public String getImdbId() {
+        return imdbId;
+    }
+
+    public void setImdbId(String imdbId) {
+        this.imdbId = imdbId;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getReleaseDate() {
+        return releaseDate;
+    }
+
+    public void setReleaseDate(String releaseDate) {
+        this.releaseDate = releaseDate;
+    }
+
+    public String getTrailerLink() {
+        return trailerLink;
+    }
+
+    public void setTrailerLink(String trailerLink) {
+        this.trailerLink = trailerLink;
+    }
+
+    public String getPoster() {
+        return poster;
+    }
+
+    public void setPoster(String poster) {
+        this.poster = poster;
+    }
+
+    public List<String> getGenres() {
+        return genres;
+    }
+
+    public void setGenres(List<String> genres) {
+        this.genres = genres;
+    }
+
+    public List<String> getBackdrops() {
+        return backdrops;
+    }
+
+    public void setBackdrops(List<String> backdrops) {
+        this.backdrops = backdrops;
+    }
+
+    public List<Review> getReviewIds() {
+        return reviewIds;
+    }
+
+    public void setReviewIds(List<Review> reviewIds) {
+        this.reviewIds = reviewIds;
+    }
 }
+
 
 
