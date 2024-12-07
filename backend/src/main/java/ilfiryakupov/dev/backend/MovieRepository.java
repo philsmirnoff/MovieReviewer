69bcd43daf5066f43ad1d55d26cd9e28dc8c3729
Ilfir Yakupov <philsmirnoff@gmail.com>
2024-12-07 03:40:22 -0500

    replaced ObjectId to ImdbId for movie search

--- backend/src/main/java/ilfiryakupov/dev/backend/MovieRepository.java
+++ backend/src/main/java/ilfiryakupov/dev/backend/MovieRepository.java
@@ -3,5 +3,8 @@ package ilfiryakupov.dev.backend;
 import org.bson.types.ObjectId;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
+import java.util.Optional;
+
 public interface MovieRepository extends MongoRepository<Movie, ObjectId> {
+    Optional<Movie> findMovieByImdbId(String imdbId);
 }
