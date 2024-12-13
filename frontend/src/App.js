756db5cf511566f254f0d22ab49f036af3512f07
Ilfir Yakupov <philsmirnoff@gmail.com>
2024-12-12 23:44:06 -0500

    Add functionality for adding and getting movie reviews

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,9 +6,13 @@ import Layout from "./components/Layout";
 import Home from "./components/Home/Home";
 import { Routes, Route } from "react-router-dom";
 import Header from "./components/Header/Header";
+import Reviews from "./components/Reviews/Reviews";
+import Trailer from "./components/Trailer/Trailer";
 
 function App() {
   const [movies, setMovies] = useState();
+  const [movie, setMovie] = useState();
+  const [reviews, setReviews] = useState([]);
 
   const getMovies = async () => {
     try {
@@ -20,10 +24,23 @@ function App() {
     }
   };
 
-
   useEffect(() => {
     getMovies();
   }, []);
+
+  const getMovieData = async (movieId) => {
+    try {
+      const response = await api.get(`/api/v1/movies/${movieId}`);
+
+      const singleMovie = response.data;
+
+      setMovie(singleMovie);
+
+      setReviews(singleMovie.reviews);
+    } catch (error) {
+      console.error(error);
+    }
+  };
 
   return (
     <div className="App">
@@ -31,6 +48,18 @@ function App() {
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route index element={<Home movies={movies} />}></Route>
+          <Route path="/Trailer/:ytTrailerId" element={<Trailer />}></Route>
+          <Route
+            path="/Reviews/:movieId"
+            element={
+              <Reviews
+                getMovieData={getMovieData}
+                movie={movie}
+                reviews={reviews}
+                setReviews={setReviews}
+              />
+            }
+          ></Route>
         </Route>
       </Routes>
     </div>
