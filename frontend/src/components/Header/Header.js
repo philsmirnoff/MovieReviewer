aff782a6ef2467aa8598b5c7fc60fd292f9df91c
Ilfir Yakupov <philsmirnoff@gmail.com>
2024-12-13 00:21:37 -0500

    clean up

--- frontend/src/components/Header/Header.js
+++ frontend/src/components/Header/Header.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faVideoSlash } from "@fortawesome/free-solid-svg-icons";
-import Button from "react-bootstrap/Button";
 import Container from "react-bootstrap/Container"
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
@@ -25,8 +24,6 @@ return (
                     <NavLink className ="nav-link" to="/">Home</NavLink>
                     <NavLink className ="nav-link" to="/watchList">Watch List</NavLink>
                 </Nav>
-                <Button variant="outline-info" className="me-2">Login</Button>
-                <Button variant="outline-info">Register</Button>
             </Navbar.Collapse>
         </Container>
     </Navbar>
